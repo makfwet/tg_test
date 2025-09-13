7c8af4d994e64908e303a22713dffd77c7240c7f
makfwet <ajwbrhfodbw@gmail.com>
2025-09-13 20:18:09 +1000

    final commit

--- telegram/handlers/not_dict_of_services.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Router, types
-from telegram.filters.is_in_dict_of_services import IsInDictOfServices
-
-
-router_not_dict_of_services = Router()
-router_not_dict_of_services.callback_query.filter(IsInDictOfServices)
